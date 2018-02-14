5c13e9a93acb93feface2f6da2a859538d7fe44a
tfstephens <tfstephens1@crimson.ua.edu>
2018-02-14 13:03:35 -0600

    MasterProject
    Thou giveth thy code to friend not foe

---
+++ Index.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<style>
+
+table, th, td {
+    border: 1px solid;
+}
+th, td {
+padding: 10px;
+}
+</style>
+<body>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="memberModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title" id="memberModalLabel">Edit Member Detail</h4>
+            </div>
+            <div class="dash">
+             <!-- Content goes in here -->
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Create Table and Table Headers --> 
+<table class="table table-hover" data-link="row">
+
+<tr>
+<!-- <td>InstitutionId</td> -->
+<td>Institution Name</td>
+<td>College Name</td>
+<td>Program Name</td>
+</tr>
+
+<?php
+require('conn.php');
+// Display table
+$sql = "SELECT a.InstitutionId, b.InstitutionName, a.CollegeName, c.ProgramName
+        FROM colleges a
+             INNER JOIN institutions b 
+                   ON a.InstitutionId = b.InstitutionId
+             INNER JOIN programs c 
+                   ON a.InstitutionId = c.InstitutionId";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    
+    // Output data of each row
+    while($row = mysqli_fetch_array($result)) {
+        echo "<tr>";
+        //echo "<td><button data-toggle='modal' data-target='#programModal' data-id='".$row["InstitutionId"]."' id='getProgram'  class='btn btn-sm btn-info'>View</button></td>";
+        echo "<td>" .$row["InstitutionName"]. "</td>";
+        echo "<td>" .$row["CollegeName"]. "</td>";
+        echo "<td>" .$row["ProgramName"]. "</td>";
+        echo '<td>
+                    <a class="btn btn-small btn-primary"
+                       data-toggle="modal"
+                       data-target="#exampleModal"
+                       data-whatever=".$row["InstitutionId"].">Edit</a>
+                 </td>';
+        echo "<tr>";
+    }
+} else {
+    echo "0 results";
+}
+ $conn->close();
+?>
+</table>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<script>
+    $('#exampleModal').on('show.bs.modal', function (event) {
+          var button = $(event.relatedTarget) // Button that triggered the modal
+          var recipient = button.data('whatever') // Extract info from data-* attributes
+          var modal = $(this);
+          var dataString = 'id=' + recipient;
+
+            $.ajax({
+                type: "GET",
+                url: "getPrograms.php",
+                data: dataString,
+                cache: false,
+                success: function (data) {
+                    console.log(data);
+                    modal.find('.dash').html(data);
+                },
+                error: function(err) {
+                    console.log(err);
+                }
+            });  
+    })
+ </script>
+</body>
+</html>
