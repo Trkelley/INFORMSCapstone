d778ef4f7bcb015a9c071c18b0db2c8b60a58ebb
tfstephens <tfstephens1@crimson.ua.edu>
2018-02-26 11:06:59 -0600

    troubleshooting

--- conn.php
+++ conn.php
@@ -27,5 +27,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     exit();
+}
 >>>>>>> a0c15be0008c620910052b429c188d9a8576d546
 ?>
