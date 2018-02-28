67b6846aeb9025602ef53e1fb04f112a2677a269
tfstephens <tfstephens1@crimson.ua.edu>
2018-02-28 09:35:12 -0600

    Fixed conn

--- conn.php
+++ conn.php
@@ -1,20 +1,6 @@
 
 <?php
 // Set Connection Variables
-$servername = "127.0.0.1";
-$username = "bama_team";
-$password = "N@tionalChampions!2017";
-$dbname = "analytic_backup_edu";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    exit();
-}
-
-// Set Connection Variables
 $servername = "ec2-54-159-222-176.compute-1.amazonaws.com";
 $username = "bama_team";
 $password = "N@tionalChampions!2017";
@@ -27,4 +13,5 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     exit();
 }
+
 ?>
