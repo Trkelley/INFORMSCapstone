a6c26d4556930bce483b78d30d3b62db1730573a
benshearer2 <36492330+benshearer2@users.noreply.github.com>
2018-03-19 12:16:47 -0500

    Added courses table
    user story 2.7 which added the courses table in the curriculum info. also made the modal wider

--- getPrograms.php
+++ getPrograms.php
@@ -3,7 +3,7 @@
 require('conn.php');
 $id = $_GET['id'];
 
- $sql = ("SELECT  i.InstitutionName, i.InstitutionCity, i.InstitutionState, i.InstitutionZip, i.InstitutionRegion,
+$sql = ("SELECT  i.InstitutionName, i.InstitutionCity, i.InstitutionState, i.InstitutionZip, i.InstitutionRegion,
     p.ProgramName, p.ProgramType, p.DeliveryMethod, p.ProgramObjectives, p.FullTimeDuration, p.PartTimeDuration, p.YearEstablished,
 	p.TestingRequirement, p.OtherRequirement, p.EstimatedResidentTuition, p.EstimatedNonresidentTuition, p.CostPerCredit, p.ProgramObjectives, p.OtherRequirement,
 	c.ContactName, c.ContactTitle, c.ContactPhone, c.ContactEmail, c.ContactId, co.CollegeName, co.CollegeType, courses.CourseTitle, courses.CourseType, pc.RequirementType
@@ -20,8 +20,6 @@ $id = $_GET['id'];
     ON pc.CourseId = courses.CourseId WHERE p.programId = $id;");
  $result = $conn->query($sql);
  $modalData = $conn->query($sql)->fetch_array();
- 
- 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -44,14 +42,13 @@ $id = $_GET['id'];
 	<p class="h3 font-weight-bold"><?php echo $modalData['InstitutionName']?> </p>
 	<p class="h4"><?php echo $modalData['CollegeName']?></p>
 	<p class="h6 text-muted"><?php echo $modalData['ProgramName']?></p>
-	
 </div>
 <form method="post" action="insert.php" role="form" id="programForm" onsubmit="return validateForm()">
 	<div class="modal-body">
 	<input type="hidden" name="contactId" value="<?php echo $modalData['ContactId']; ?>">
 <!-- Contact Information -->
 	<p>
-  <button class="btn btn-primary" style="width:870px; height:40px;" type="button" data-toggle="collapse" data-target="#ContactInfoCollapse" aria-expanded="false" aria-controls="ContactInfoCollapse">
+  <button class="btn btn-primary btn-block" type="button" data-toggle="collapse" data-target="#ContactInfoCollapse" aria-expanded="false" aria-controls="ContactInfoCollapse">
     Contact Information
   </button>
 </p>
@@ -78,7 +75,7 @@ $id = $_GET['id'];
 
 <!-- Progam General Information -->
 <p>
-<button class="btn btn-primary" style="width:870px; height:40px;" type="button" data-toggle="collapse" data-target="#ProgramGeneralInfoCollapse" aria-expanded="false" aria-controls="ProgramGeneralInfoCollapse">
+<button class="btn btn-primary btn-block" type="button" data-toggle="collapse" data-target="#ProgramGeneralInfoCollapse" aria-expanded="false" aria-controls="ProgramGeneralInfoCollapse">
     Program General Information
   </button>
   </p>
@@ -100,11 +97,14 @@ $id = $_GET['id'];
 		    	<option>Other</option>
 		    </select> 
 		</div>
-	
+		
+		<fieldset disabled>
 		<div class="form-group">
 		    <label for="i.InstitutionCity">Program City</label>
 		    <input type="text" class="form-control" id="InstitutionCity" name="InstitutionCity" value="<?php echo $modalData['InstitutionCity'];?>"/>
 		</div>
+		</fieldset>
+		<fieldset disabled>
 		<div class="form-group">
 		    <label for="i.InstitutionState">Program State</label>
 		    <select class="form-control" id="InstitutionState" name="InstitutionState" >
@@ -161,7 +161,7 @@ $id = $_GET['id'];
 		    	<option>WY</option>
 		    </select>
 		</div>
-		
+		</fieldset>
 		<fieldset disabled>
 		<div class="form-group">
 		<label for="i.InstitutionRegion">Program Region</label>
@@ -172,29 +172,14 @@ $id = $_GET['id'];
     		<option>Northeast</option>
     		<option>West</option>
     		<option>Other</option>
-		</select> 
+		</select>
 		</div>
 		</fieldset>
 		<div class="form-group">
 		    <label for="p.ProgramName">Program Name</label>
 		    <input type="text" class="form-control" id="programName" name="programName" value="<?php echo $modalData['ProgramName'];?>"/>
 		</div>
-		<div class="form-group">
-		    <label for="co.CollegeType">College Type</label>
-
-		    <select class="form-control" id="CollegeType" name="CollegeType" >
-		    	<option value="<?php echo $modalData['CollegeType'];?>"><?php echo $modalData['CollegeType'];?></option>
-		    	<option>Arts and Sciences</option>
-		    	<option>Business</option>
-		    	<option>Center or Institute</option>
-		    	<option>Engineering</option>
-		    	<option>Informatics</option>
-		    	<option>Multiple Schools</option>
-		    	<option>Professional Studies</option>
-		    	<option>Information Systems &amp; Management</option>
-		    </select>
-		    
-		</div>
+		
 		<div class="form-group">
 		    <label for="p.YearEstablished">Year Established</label>
 		    <input type="text" class="form-control" id="YearEstablished" name="YearEstablished" value="<?php echo $modalData['YearEstablished'];?>"/>
@@ -204,7 +189,7 @@ $id = $_GET['id'];
 
 <!-- Program Details -->
 <p>
-<button class="btn btn-primary" style="width:870px; height:40px;" type="button" data-toggle="collapse" data-target="#ProgramDetailsCollapse" aria-expanded="false" aria-controls="ProgramDetailsCollapse">
+<button class="btn btn-primary btn-block" type="button" data-toggle="collapse" data-target="#ProgramDetailsCollapse" aria-expanded="false" aria-controls="ProgramDetailsCollapse">
     Program Details
   </button>
   </p>
@@ -219,7 +204,7 @@ $id = $_GET['id'];
 		    <input type="text" class="form-control" id="" name="" value=""/>
 		</div>
 		<div class="form-group">
-		    <label for="p.ProgramType">Program Type</label>
+		<label for="p.ProgramType">Program Type</label>
 		<select class="form-control" id="ProgramType" name="ProgramType"  >
     		<option value="<?php echo $modalData['ProgramType'];?>"><?php echo $modalData['ProgramType'];?></option>
     		<option>B.B.A</option>
@@ -258,14 +243,14 @@ $id = $_GET['id'];
 		</div>
 		<div class="form-group">
 		    <label for="p.OtherRequirement">Other Requirements</label>
-		   <textarea  maxlength = "255" class="form-control" rows="6" id="OtherRequirement" name="OtherRequirement"><?php echo $modalData['OtherRequirement'];?></textarea>
+		    <textarea  maxlength = "255" class="form-control" rows="6" id="OtherRequirement" name="OtherRequirement"><?php echo $modalData['OtherRequirement'];?></textarea>
 		</div>
   </div>
 </div>
 
 <!-- Curriculum Information -->
 <p>
-<button class="btn btn-primary" style="width:870px; height:40px;" type="button" data-toggle="collapse" data-target="#CurriculumInfoCollapse" aria-expanded="false" aria-controls="CurriculumInfoCollapse">
+<button class="btn btn-primary btn-block" type="button" data-toggle="collapse" data-target="#CurriculumInfoCollapse" aria-expanded="false" aria-controls="CurriculumInfoCollapse">
     Curriculum Information
   </button>
   </p>
@@ -275,37 +260,61 @@ $id = $_GET['id'];
        <div class="card card-body">
         <div id="CurriculumInfoCollapse" class="collapse"> 
         <div class="form-group">
-        </div>                           
-
-            <table class="table table-striped table-bordered">
+        
+            <table class="table table-striped table-bordered" id = "currTable">
             <thead>
                 <tr>
                     <th>Course Title</th>
                     <th>Course Type</th>
-                    <th>Course Department</th>
+                    <th>Discipline</th>
+                    <th>Delete</th>
                 </tr>
                 </thead>
                 <tbody>
                     <tr>
 <?php
 // Output data of each row
-while($row = mysqli_fetch_assoc($result)){
+$rowCount = 2;
+while($row = mysqli_fetch_assoc($result))
+{
+    $rowCount++;
         echo '<tr>';
-        echo '<td>' .$row['CourseTitle']. '</td>';
         echo '<td>';
-        echo '<select><option value= "'.$row['RequirementType'].'">'.$row['RequirementType'].'</option>
+        echo '<input class="form-control" type = "text" value = "'.$row['CourseTitle'].'" name = "courseTitle"/>';
+        echo "</td>";
+        
+        echo '<td>';
+        echo '<select class="form-control" > class="form-control" <option value= "'.$row['RequirementType'].'">'.$row['RequirementType'].'</option>
         <option>Required</option>
         <option>Elective</option>';
         echo "</td>";
-        echo '<td>' .$row['ProgramName']. '</td>';
-        echo '<tr>';
+        
+        echo '<td>';
+        echo '<select class="form-control" > <option value= ""></option>
+        <option>Information Systems</option>
+        <option>Operations Research</option>
+        <option>Statistics</option>';
+        echo "</td>";
+
+        echo '<td>
+                    <a class="btn btn-small btn-primary"
+                       data-toggle=""
+                       data-target=""
+                       data-whatever="">Delete</a>
+                 </td>';
+ 
 };
         $result->close();
 
 ?>
                     </tr>
-                </tbody>
+                </tbody>               
             </table>
+            <br>                         
+    <div style = "text-align:right; margin-right: 10%;" >
+            <a type = "button" onclick = "addCourse()">Add Course</a>
+            </div>           
+    </div>   
         </div>
     </div>
 
@@ -313,7 +322,7 @@ while($row = mysqli_fetch_assoc($result)){
 
 <!-- Submission -->
   <p>
-<button class = "btn btn-primary" style="width:870px; height:40px;" type="submit" value="Update" data-toggle="collapse" data-target="#SubmissionCollapse" aria-expanded="false" aria-controls="SubmissionCollapse">
+<button class = "btn btn-primary btn-block" type="submit" value="Update" data-toggle="collapse" data-target="#SubmissionCollapse" aria-expanded="false" aria-controls="SubmissionCollapse">
 Submit</button>
   </p>
 </div>
@@ -322,9 +331,9 @@ Submit</button>
 //function validateForm() {
 //var cn = document.forms["myForm"]["contactName"].value;
 
-// if (cn == "") {
-//     alert("Contact Name must be filled out");
-//     return false;
+//   if (cn == "") {
+//       alert("Contact Name must be filled out");
+//       return false;
 //}
 
 $('.phone')
@@ -389,57 +398,73 @@ var cp = document.forms["programForm"]["contactPhone"].value; var inputValcp = d
 var ce = document.forms["programForm"]["contactEmail"].value; var inputValce = document.getElementById("contactEmail");
 
 if (cn == "") {
-alert("Contact Name must be filled out");
-inputValcn.style.border="1px solid red";
-return false;
+  alert("Contact Name must be filled out");
+  inputValcn.style.border="1px solid red";
+  return false;
 }
 if (ct == ""){
 	alert("Contact Title must be filled out");
 	inputValct.style.border="1px solid red";
-return false;
+  return false;
 }
 if (cp == ""){
-	alert("Contact Phone must be filled out");
-	inputValcp.style.border="1px solid red";
-return false;
-}
+ 	alert("Contact Phone must be filled out");
+ 	inputValcp.style.border="1px solid red";
+  return false;
+ }
 if (ce == ""){
- alert("Contact Email must be filled out");
- inputValce.style.border="1px solid red";
- return false;
-}
+   alert("Contact Email must be filled out");
+   inputValce.style.border="1px solid red";
+   return false;
+  }
 var email = $("#contactEmail").val();
 if (validateEmail(email) == false)
 {
 	 alert("Contact Email must be filled out with a valid email");
- inputValce.style.border="1px solid red";
- return false;
+   inputValce.style.border="1px solid red";
+   return false;
 }
 var pn = document.forms["programForm"]["programName"].value; var inputValpn = document.getElementById("programName");
 if (pn == ""){
-	alert("Program Name must be filled out");
-	inputValpn.style.border="1px solid red";
- return false;
-}
+  	alert("Program Name must be filled out");
+  	inputValpn.style.border="1px solid red";
+   return false;
+  }
 var po = document.forms["programForm"]["ProgramObjectives"].value; var inputValpo = document.getElementById("ProgramObjectives");
 if (po == ""){
-	alert("Program Objectives must be filled out");
-	inputValpo.style.border="1px solid red";
- return false;
-}
+  	alert("Program Objectives must be filled out");
+  	inputValpo.style.border="1px solid red";
+   return false;
+  }
 else
 {
-SubmissionFunction();
-return true;
+  SubmissionFunction();
+  return true;
 }
 }
 function SubmissionFunction() {
 	alert("Your data has been submitted for approval.");
 }
 
+function addCourse(){
+	var table = document.getElementById("currTable");
+    var row = table.insertRow(<?php echo $rowCount?>);
+    var courseTitle = row.insertCell(0);
+    var courseType = row.insertCell(1);
+    var courseDisclipline = row.insertCell(2);
+    var courseDelete = row.insertCell(3);
+    courseTitle.innerHTML ='<input class="form-control" type = "text" name = "courseTitle"/>';
+    courseType.innerHTML = '<select class="form-control"><option></option><option>Required</option><option>Elective</option>';
+    courseDisclipline.innerHTML = '<select class="form-control"><option></option><option>Information Systems</option><option>Operations Research</option><option>Statistics</option>';
+    courseDelete.innerHTML = '<a class="btn btn-small btn-primary" data-toggle="" data-target="" data-whatever="">Delete</a>';
+}
+
+
+
 </script>
  
 	</form>
+	
 <?php if (isset($_POST['submit'])) {
     
  $contactId = $_POST['ContactId'];
@@ -466,7 +491,6 @@ function SubmissionFunction() {
 
 }
 ?>
-
 </body>
 </html>
 
