8033e3d9988b3d04526f34be268ded1d1e38fef1
tfstephens <tfstephens1@crimson.ua.edu>
2018-03-19 09:29:33 -0500

    Phone Format refinement
    Phone format now corrects as user types

--- getPrograms.php
+++ getPrograms.php
@@ -67,7 +67,7 @@ $id = $_GET['id'];
 		</div>
 		<div class="form-group">
 		    <label for="c.ContactPhone">Contact Phone</label>
-		    <input type="text" class="form-control phone" id="contactPhone" name="contactPhone" value="<?php echo $modalData['ContactPhone'];?>"/>
+		    <input type="text" class="form-control phone" maxlength="14" id="contactPhone" name="contactPhone" value="<?php echo $modalData['ContactPhone'];?>"/>
 		</div>
 		<div class="form-group">
 		    <label for="c.ContactEmail">Contact Email</label>
@@ -326,11 +326,56 @@ Submit</button>
 //     alert("Contact Name must be filled out");
 //     return false;
 //}
-$(".phone").on('change', function() {
-        var number = $(this).val()
-        number = number.replace(/(\d{3})(\d{3})(\d{4})/, "($1)-$2-$3");
-        $(this).val(number)
-    });
+
+$('.phone')
+.on('keypress', function(e) {
+  var key = e.charCode || e.keyCode || 0;
+  var phone = $(this);
+  if (phone.val().length === 0) {
+    phone.val(phone.val() + '(');
+  }
+  // Auto-format- do not expose the mask as the user begins to type
+  if (key !== 8 && key !== 9) {
+    if (phone.val().length === 4) {
+      phone.val(phone.val() + ')');
+    }
+    if (phone.val().length === 5) {
+      phone.val(phone.val() + ' ');
+    }
+    if (phone.val().length === 9) {
+      phone.val(phone.val() + '-');
+    }
+    if (phone.val().length >= 14) {
+      phone.val(phone.val().slice(0, 13));
+    }
+  }
+
+  // Allow numeric (and tab, backspace, delete) keys only
+  return (key == 8 ||
+    key == 9 ||
+    key == 46 ||
+    (key >= 48 && key <= 57) ||
+    (key >= 96 && key <= 105));
+})
+
+.on('focus', function() {
+  phone = $(this);
+
+  if (phone.val().length === 0) {
+    phone.val('(');
+  } else {
+    var val = phone.val();
+    phone.val('').val(val); // Ensure cursor remains at the end
+  }
+})
+
+.on('blur', function() {
+  $phone = $(this);
+
+  if ($phone.val() === '(') {
+    $phone.val('');
+  }
+});
 
 function validateEmail(contactEmail) {
 var re = /\S+@\S+\.\S+/;
