b79fa1cb98be3a2099fbb666408e689a1ded7291
Collier Kauffman <36460706+jckauffman1@users.noreply.github.com>
2018-04-12 17:18:08 -0500

    Final Login Documentation

--- login.php
+++ login.php
@@ -1,6 +1,16 @@
+<!-- This the login page for University administrators. --> 
+<!-- University administrators can create an account with INFORMS through the "Create Account" button. --> 
+<!-- University administrators can click "Forgot Password" and a process will ensue to create a new password for their account. -->
+
+<!-- After the Login form is submitted, the Login data will be taken to AUTHENTICATE.PHP. 
+AUTHENTICATE.PHP will validate the Email address and Password by matching the input values with values in the database. -->
+
+<!-- The function for "Create Account" HAS NOT been coded yet. -->
+ 
 <html lang="en">
 <img class="irc_mi" src="https://www.informs.org/var/ezflow_site/storage/images/media/or-ms-today/images/0217/new-informs-logo2/3648939-1-eng-US/New-INFORMS-logo.jpg" onload="typeof google==='object'&amp;&amp;google.aft&amp;&amp;google.aft(this)" width="350" height="75" alt="Image result for Informs">
 
+	<!-- Styling & Design -->
     <meta charset="utf-8"></meta>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
@@ -15,7 +25,6 @@
      padding: 20px 400px;
     }
 form {border: 3px solid #f1f1f1;}
-
 input[type=text], input[type=password]{
     width: 100%;
     padding: 12px 20px;
@@ -24,13 +33,11 @@ input[type=text], input[type=password]{
     border: 1px solid #ccc;
     box-sizing: border-box;
 }
-
 button {
 padding: 12px 20px;
 margin: 8px 0;
 width: 72%;
 }
-
 </style>
 </head>
 <body>
@@ -38,6 +45,7 @@ width: 72%;
 <!-- Sign Up Button -->
 <div style="text-align:right; margin-right: 10%;"><a class="btn btn-small btn-primary">Create Account</a></div>;
 
+<!-- Page Header -->
 <h1 class="text-center" class="page-header" id="loginModalLabel">Program Administrator Login</h1>
   
   <form method="post" action="authenticate.php" id="loginForm" onsubmit="return validate()">
@@ -47,16 +55,22 @@ width: 72%;
   		<div class="modal-dialog modal-lg">
 			<div class="model-content modal-lg">
 				<div class="modal-header modal-lg">
+				
+					<!-- Email Input -->
 					<label for="email"><b>Email Address</b></label>
 					<input class="form-control" type="text" placeholder="Enter E-mail Address" id="email" name="email" required>
 					
+					<!-- Password Input -->
 					<label for="psw"><b>Password</b></label>
 					<input class="form-control" type="password" placeholder="Enter Password" id="psw" name="psw" required>
 					<p></p>
 					
 					<div class="container">
+					
+					<!-- Submit Button -->
 					<button class="btn btn-small btn-primary" type="submit" onclick="validate()">Login</button>
 					
+					<!-- Forgot Password Button -->
 					<button class="btn btn-small btn-primary" type="button">Forgot Password</button>
 					</div>
 				</div>
@@ -66,28 +80,34 @@ width: 72%;
   </form>
   
   <script>
-  
-  function validateEmail(email) {
+
+	// This function checks for email characters.
+	function validateEmail(email) {
 	  var re = /\S+@\S+\.\S+/;
 	  return re.test(email);
 	  }
-  
+
+  	// This function validates the user's inputs and checks for errors.
+  	// It is called upon the click of the Submit "Login" button.
 	function validate(){
-		var em = document.forms["loginForm"]["email"].value; var inputValem = document.getElementById("email");
-		var pw = document.forms["loginForm"]["psw"].value; var inputValpw = document.getElementById("psw");
-		
+		var em = document.forms["loginForm"]["email"].value; var inputValem = document.getElementById("email"); // Setting Email Input to email variable.
+		var pw = document.forms["loginForm"]["psw"].value; var inputValpw = document.getElementById("psw");	// Setting Password Input to password variable. 
+
+		// Validating that the Email input box has characters in it.
 		if (em == ""){
 			alert("You must provide your email address.");
 			inputValem.style.border = "1px solid red";
 			return false;
 		}
-		
+
+		// Validating that the Password input box has characters in it.
 		if (pw == ""){
 			alert("You must provide your password.");
 			inputValpw.style.border = "1px solid red";
 			return false;
 		}
-		
+
+		// Validating that the Email address is a real email address. 
 		var email = $("#email").val();
 		if (validateEmail(email) == false)
 		{
@@ -97,6 +117,8 @@ width: 72%;
 		}
 		
 	}
+
+	// CREATE ACCOUNT FUNCTION COULD GO HERE
 		
   </script>
   
