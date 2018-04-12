e7f5f8a2919935435e6464a2a833595684648197
Collier Kauffman <36460706+jckauffman1@users.noreply.github.com>
2018-04-12 17:14:01 -0500

    Final Authentication Documentation

--- authenticate.php
+++ authenticate.php
@@ -1,4 +1,18 @@
+<!-- This is the authentication file that will take care of all the back-end database functions involved with the Login page. -->
+
+<!-- CREATE ACCOUNT back-end functions could also be performed in this file. -->
+
+<!-- A NEW TABLE WILL NEED TO BE CREATED IN THE DATABASE FOR VALIDATING EMAIL AND PASSWORD -->
+
 <?php
+
+// This redirects the page to Index.php where University administrators can make program changes.
 header("location:Index.php");
+
+// VALIDATE EMAIL ADDRESS
+
+// VALIDATE PASSWORD
+
+// VALIDATE CREATION OF NEW ACCOUNT
 
 ?>
