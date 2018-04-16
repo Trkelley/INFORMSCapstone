1695f03ce85a8fe8b30944e3128c3254da3cbedc
tfstephens <tfstephens1@crimson.ua.edu>
2018-04-16 18:55:31 -0500

    Connection Final Push

--- conn.php
+++ conn.php
@@ -1,7 +1,6 @@
 <!-- THIS FILE CONTAINS THE NECESSARY DATABASE CONNECTIONS -->
 
 <!-- This file is called in many of the other files in order to establish a database connection. -->
-
 <?php
 // Set Connection Variables
 $servername = "ec2-54-159-222-176.compute-1.amazonaws.com";
@@ -11,10 +10,10 @@ $dbname = "analytic_backup_edu";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     exit();
 }
+
 ?>
