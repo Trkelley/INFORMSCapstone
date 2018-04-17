53c8c2d1fa8b574c9ab3e867463a362db595cabb
tfstephens <tfstephens1@crimson.ua.edu>
2018-04-16 19:40:17 -0500

    Insert Final Push

--- insert.php
+++ insert.php
@@ -5,50 +5,34 @@ session_start();
 require('conn.php');
 
 //displays information if the values are passed through without sending to the database
-echo $_POST['contactName'];
-echo "<br>";
-echo $_POST['contactTitle'];
-echo "<br>";
-echo $_POST['contactPhone'];
-echo "<br>";
-echo $_POST['contactEmail'];
-echo "<br>";
-echo $_POST['programId'];
-echo "<br>";
-echo $_POST['programType'];
-echo "<br>";
-echo $_POST['programAccess'];
-echo "<br>";
-echo $_POST['programObjectives'];
-echo "<br>";
-echo $_POST['institutionName'];
-echo "<br>";
-echo "<br>";
-echo $_POST['programName'];
-echo "<br>";
-echo $_POST['collegeName'];
-echo "<br>";
-echo $_POST['institutionState'];
-echo "<br>";
-echo $_POST['institutionZip'];
-echo "<br>";
-echo $_POST['institutionPhone'];
-echo "<br>";
-echo $_POST['institutionEmail'];
-echo "<br>";
-echo $_POST['institutionAccess'];
-echo "<br>";
-echo $_POST['collegeId'];
-echo "<br>";
-echo $_POST['collegeName'];
-echo "<br>";
-echo $_POST['collegeType'];
-echo "<br>";
-echo $_POST['rowCount'];
+$contactName = $_POST['contactName'];
+$contactTitle = $_POST['contactTitle'];
+$contactPhone = $_POST['contactPhone'];
+$contactEmail = $_POST['contactEmail'];
+$programId = $_POST['programId'];
+$programType = $_POST['programType'];
+$programAccess = $_POST['programAccess'];
+$programObjectives = $_POST['programObjectives'];
+$institutionName = $_POST['institutionName'];
+$institutionId = $_POST['institutionId'];
+$contactId = $_POST['contactId'];
+$programName = $_POST['programName'];
+$collegeName = $_POST['collegeName'];
+$institutionState = $_POST['institutionState'];
+$deliveryMethod = $_POST['deliveryMethod'];
+$fullTimeDuration = $_POST['fullTimeDuration'];
+$partTimeDuration = $_POST['partTimeDuration'];
+$testingRequirement = $_POST['testingRequirement'];
+$otherRequirement = $_POST['otherRequirement'];
+$_OtherRequirement = mysqli_real_escape_string($conn, $otherRequirement);
+$credits = $_POST['credits'];
+$yearEstablished = $_POST['yearEstablished'];
+$scholarship = $_POST['scholarship'];
+$estimatedResidentTuition = $_POST['estimatedResidentTuition'];
+$estimatedNonresidentTuition = $_POST['estimatedNonresidentTuition'];
+$costPerCredit = $_POST['costPerCredit'];
 
-echo "<pre>";
-print_r($_SESSION);
-echo "</pre>";
+/*Session Variables*/
 $courseIdList = $_SESSION['courseIdList'];
 $instructorIdList = $_SESSION['instructorIdList']; //empty
 $courseNumberList = $_SESSION['courseNumberList'];
@@ -59,83 +43,97 @@ $syllabusFileList = $_SESSION['syllabusFileList']; //empty
 $syllabusFilesizeList = $_SESSION['syllabusFilesizeList']; //empty
 $analyticTagList = $_SESSION['analyticTagList']; //Full of zeroes
 $businessTagList = $_SESSION['businessTagList']; //empty
-$courseTypeList = $_SESSION['courseTitleList']; //empty
-
-//Passing the course titles. You can pass them individually (ex: echo $courseTitleList[2]) or in a foreach loop
+$date = date('m/d/Y');
 $courseTitleList = $_POST['courseTitle'];
-foreach($courseTitleList as  $courseTitles)
-{
-    echo $courseTitles;
-}
-//Passing the course requirements. You can pass them individually (ex: echo $courseRequirementList[2]) or in a foreach loop
 $requirementList = $_POST['requirementType'];
-foreach ($requirementList as $requirements){
-    echo $requirements;
-}
-//Passing the course disciplines. You can pass them individually (ex: echo $disciplineList[2]) or in a foreach loop
 $disciplineList = $_POST['courseDiscipline'];
-foreach ($disciplineList as $disciplines){
-    echo $disciplines;
-}
-//Passing delete course checkbox, returns as either "on" or NULL
+if(isset($_POST['deleteCourseBx']))
+{
 $deleteCourseAction = $_POST['deleteCourseBx'];
-foreach ($deleteCourseAction as $deletions){
-    echo $deletions;
 }
-$x  = 3;
-$rowCount = $_POST['rowCount'];
-while ($x <= $rowCount){
+else{
+    $deleteCourseAction = range(0,100,1);
+}
+/* Inserting program information */
     
-    /* $queryCourses = "INSERT INTO courses ( InstructorId, CourseNumber, CourseTitle, DeliveryMethod, HasCapstoneProject, CourseText, SyllabusFile, SyllabusFilesize, AnalyticTag, BusinessTag, ReferenceId, UpdateType, LastUpdate)
-     VALUES('$instructorIdList[$x]' , '$courseNumberList[$x]' , '$courseTitleList[$x]', '$deliveryMethodList[$x]', '$hasCapstoneProjectList[$x]', '$courseTextList[$x]', '$syllabusFileList[$x]', '$syllabusFilesizeList[$x]', '$analyticTagList[$x]', '$businessTagList[$x]' , '$courseIdList[$x]', 2 , now(), '$courseTypeList[$x]');";
-     
-     mysqli_query( $conn, $queryCourses) or die('Did not enter Course Info'); */
-    echo "<br>";
-    $x++;
+    $insertCollege = "
+    INSERT INTO colleges (InstitutionId, CollegeName, CollegeType, ReferenceId, UpdateType, LastUpdate)
+    VALUES($_POST[institutionId] , '$_POST[collegeName]' , '$_POST[collegeType]', $_POST[collegeId], 2 , now());";
+
+    mysqli_query( $conn, $insertCollege) or die('Did not enter college Info');
+    
+    $insertContact = "
+    INSERT INTO contacts(ContactName, ContactTitle, ContactPhone, ContactEmail, ReferenceId, UpdateType, LastUpdate) 
+    VALUES('$contactName', '$contactTitle', '$contactPhone', '$contactEmail', $contactId, 2, now());";
+    
+    mysqli_query( $conn, $insertContact) or die('Did not enter contact Info'); 
+    
+$number = 2;
+$insertProgram = 'INSERT INTO programs(InstitutionId, ContactId, ProgramName, ProgramType, DeliveryMethod, ProgramAccess, ProgramObjectives, FullTimeDuration,
+    PartTimeDuration, TestingRequirement, OtherRequirement, YearEstablished, ReferenceId, UpdateType, LastUpdate)
+    VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,now())';
+    if($query = $conn->prepare($insertProgram))
+    {
+        $query->bind_param("iisssssssssiii", $institutionId, $contactId, $programName, $programType, $deliveryMethod, $programAccess, $programObjectives, $fullTimeDuration,
+            $partTimeDuration, $testingRequirement,$otherRequirement, $yearEstablished, $programId, $number);
+        $query->execute();
+        $query->close();
+    }
+    else{
+        $error = $conn->errno . ' ' . $conn->error;
+        echo $error; 
+    } 
+   
+$x  = 0;
+$rowCount = $_POST['rowCount'];
+for ($i = 0; $i <= $rowCount; $i ++)
+{
+    if (!isset($deleteCourseAction[$i]))
+        $deleteCourseAction[$i] = 0;
 }
+ while ($x <= $rowCount){
+     if($deleteCourseAction[$x] == "on")
+     {
+         $deleteCourseAction[$x] = 3;
+     }
+     else{
+         $deleteCourseAction[$x] = 2;
+     }
+     $initialRowCount = count($courseIdList);
+     if($x >= $initialRowCount){
+         $deleteCourseAction[$x] = 1;
+         $insertNewCourse = "
+             INSERT INTO courses (CourseTitle, DeliveryMethod, UpdateType, LastUpdate)
+             VALUES('$courseTitleList[$x]', '$deliveryMethodList[$x]', 1 , now());";
+            
+         mysqli_query( $conn, $insertNewCourse) or die(mysqli_error($conn));
+          
+         /*A major concern with the line of SQL below, is the lack of unique parameters available to condense our search to one result.
+          * Since we are only gathering CourseTitle as the only new field in the course portion of the Modal, this code can break if multiple new courses exist
+          * that share the same CourseTitle. A suggestion would be to gather CourseNumbers to decrease the chance of multiple results*/
+         $addedCourseId = "SELECT courseId FROM courses WHERE UpdateType = 1 and CourseTitle = $courseTitleList[$x];";
+           
+         $insertProgram_courses = " INSERT INTO program_courses(ProgramId, CourseId, Concentration, RequirementType, UpdateType, LastUpdate, ReferenceId)
+             VALUES('$programId', '$courseIdList[$x]','$disciplineList[$x]','$requirementList[$x]', $deleteCourseAction[$x] , now(), '$courseIdList[$x]');";
 
-
-echo "<br>";
-
-
-/* $queryContacts ="INSERT INTO contacts (ContactName, ContactTitle, ContactPhone, ContactEmail, ReferenceId, UpdateType, LastUpdate)
- VALUES ('$_POST[contactName]' ,'$_POST[contactTitle]' , '$_POST[contactPhone]' , '$_POST[contactEmail]', '$_POST[contactId]', 2 , now());";
- 
- mysqli_query( $conn , $queryContacts) or die('Did not enter Contact Info');
- 
- $queryPrograms = "INSERT INTO programs (ProgramName, ProgramType, DeliveryMethod, ProgramAccess, ProgramObjectives, FullTimeDuration, PartTimeDuration, TestingRequirement, OtherRequirement, Credits, YearEstablished, Scholarship, EstimatedResidentTuition, EstimatedNonResidentTuition, CostPerCredit, ContactId, InstitutionId, ReferenceId, UpdateType, LastUpdate)
- VALUES('$_POST[programName]' , '$_POST[programType]' , '$_POST[deliveryMethod]', '$_POST[programAccess]', '$_POST[programObjectives]', '$_POST[fullTimeDuration]', '$_POST[partTimeDuration]', '$_POST[testingRequirement]', '$_POST[otherRequirement]', '$_POST[credits]', '$_POST[yearEstablished]', '$_POST[scholarship]', '$_POST[estimatedResidentTuition]', '$_POST[estimatedNonresidentTuition]', '$_POST[costPerCredit]', '$_POST[contactId]', '$_POST[institutionId]', '$_POST[programId]', 2 , now());";
- 
- mysqli_query( $conn, $queryPrograms) or die('Did not enter Program Info');
- 
- $queryInstitutions = "INSERT INTO institutions (InstitutionName, InstitutionAddress, InstitutionCity, InstitutionState, InstitutionZip, InstitutionPhone, InstitutionEmail, InstitutionAccess, ReferenceId, UpdateType, LastUpdate)
- VALUES('$_POST[institutionName]' , '$_POST[institutionAddress]' , '$_POST[institutionCity]', '$_POST[institutionState]', '$_POST[institutionZip]', '$_POST[institutionPhone]', '$_POST[institutionEmail]', '$_POST[institutionAccess]',  '$_POST[institutionId]', 2 , now());";
- 
- mysqli_query( $conn, $queryInstitutions) or die('Did not enter Institution Info');
- 
- $queryColleges = "INSERT INTO colleges ( InstitutionId, CollegeName, CollegeType, ReferenceId, UpdateType, LastUpdate)
- VALUES('$_POST[institutionId]' , '$_POST[collegeName]' , '$_POST[collegeType]', '$_POST[collegeId]', 2 , now());";
- 
- mysqli_query( $conn, $queryColleges) or die('Did not enter College Info'); */
-/*While(x < $rowcount; x++){
- * ---This If statement checks to see if the "delete" checkbox has been checked, if so, inserts a course with the updateType "3" for delete---
- * if($checkboxvariable == True){
- 
- }
- else{
- 
- }
- };
- } */
-/* $queryCourses = "INSERT INTO courses ( InstructorId, CourseNumber, CourseTitle, DeliveryMethod, HasCapstoneProject, CourseText, SyllabusFile, SyllabusFilesize, AnalyticTag, BusinessTag, ReferenceId, UpdateType, LastUpdate)
- VALUES('$_POST[instructorId]' , '$_POST[courseNumber]' , '$_POST[courseTitle]', '$_POST[deliveryMethod]', '$_POST[hasCapstoneProject]', '$_POST[courseText]', '$_POST[syllabusStyle]', '$_POST[syllabusFileSize]', '$_POST[analyticTag]', '$_POST[businessTag]' , '$_POST[courseId]', 2 , now());";
- 
- mysqli_query( $conn, $queryCourses) or die('Did not enter Course Info'); */
-
-
-//The msql_query should look like this...
-
-//$conn has been left out for testing
+         mysqli_query( $conn, $insertProgram_courses) or die('Did not add program_courses Info');
+         $x++;
+     }
+     else{
+     $insertProgram_Courses = "
+     INSERT INTO program_courses(ProgramId, Concentration, RequirementType, UpdateType, LastUpdate, ReferenceId)
+     VALUES('$programId','$disciplineList[$x]','$requirementList[$x]', $deleteCourseAction[$x] , now(), $courseIdList[$x]);";
+     mysqli_query( $conn, $insertProgram_Courses) or die(mysqli_error($conn));
+    
+     $insertCourses = "INSERT INTO courses ( InstructorId, CourseNumber, CourseTitle, DeliveryMethod, CourseText, SyllabusFile, SyllabusFilesize, 
+     AnalyticTag, BusinessTag, ReferenceId, UpdateType, LastUpdate)
+     VALUES('$instructorIdList[$x]' , '$courseNumberList[$x]' , '$courseTitleList[$x]', '$deliveryMethodList[$x]', 
+     '$courseTextList[$x]', '$syllabusFileList[$x]', '$syllabusFilesizeList[$x]', '$analyticTagList[$x]', '$businessTagList[$x]' , '$courseIdList[$x]', 2 , now());";
+         
+     mysqli_query( $conn, $insertCourses) or die(mysqli_error($conn)); 
+     $x++;
+     }
+ } 
 
 header("location:Index.php");
 ?>
